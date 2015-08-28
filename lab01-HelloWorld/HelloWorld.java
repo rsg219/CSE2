7b2d106bc6c4ad3130b7a88f20e8f90c354c00d9
rsg219 <rsg219@lehigh.edu>
2015-08-28 13:49:14 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+// Robert Gould
+//Lab 1
+//Hello World Java Program
+
+public class HelloWorld{
+    
+    public static void main(String[] args) {
+        
+        System.out.println( "#########################");
+        System.out.println( "#########################");
+        System.out.println( "#                       #");
+        System.out.println( "#  Hello World 2014!!   #");
+        System.out.println( "#                       #");
+        System.out.println( "#########################");
+        System.out.println( "#########################");
+        System.out.println("test");
+    }
+}
