b2a0d569108ccb9cf5ec49f3ee322fff84d09dcb
rsg219 <rsg219@lehigh.edu>
2015-09-02 23:44:53 +0000

    comment

---
+++ Loose/DoNow.java
@@ -0,0 +1,14 @@
+public class DoNow{
+    
+    public static void main(String[] args) 
+    {
+        
+    float Balance = 205.12f;
+    float Interest = 1.03f;
+    float NewBalance;
+
+    NewBalance = Balance * Interest;
+    
+    System.out.println(NewBalance);
+    }
+}
---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+// Robert Gould
+//HomeWork 1
+//Hello World Java Program
+
+public class WelcomeClass{
+    
+    public static void main(String[] args) {
+        
+        System.out.println( "  -----------");
+        System.out.println( "  | WELCOME |");
+        System.out.println( "   -----------" );
+        System.out.println( "  ^  ^  ^  ^  ^  ^");
+        System.out.println( " / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println( "|-R--S--G--2--1--9-|");
+        System.out.println( " \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println( "  v  v  v  v  v  v");
+        System.out.println( "This August my wife and I took an eleven-day, 675-mile bike");
+        System.out.println( "tour on our tandem. We circled Lake Ontario.");
+    }
+}
